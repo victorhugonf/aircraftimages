80327ecc6384461614678c0b21aa8b266ae2d5a4
Victor <victor.dsn.shop@alterdata.com.br>
2020-01-29 15:53:39 -0300

    Melhorias diversas

--- src/main/java/io/github/victorhugonf/aircraftimages/controller/AircraftImagesController.java
+++ src/main/java/io/github/victorhugonf/aircraftimages/controller/AircraftImagesController.java
@@ -6,71 +6,16 @@ import com.gargoylesoftware.htmlunit.html.DomNodeList;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import io.github.victorhugonf.aircraftimages.controller.dto.AircraftImageDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/aircraftimages")
 public class AircraftImagesController {
-/*
-    private JetPhotosFeign jetPhotosFeign;
-
-    public AircraftImagesController(){
-        jetPhotosFeign = Feign.builder()
-                .decoder(new HtmlDecoder())
-                .target(JetPhotosFeign.class, "https://www.jetphotos.com");
-    }
-*/
-   /* @GetMapping("/{registration}")
-    public ResponseEntity<String> byRegistration(@PathVariable String registration) throws IOException {
-        //String jetPhotosResult = jetPhotosFeign.getByRegistration(registration);
-        //System.out.println(jetPhotosResult);
-
-        String url = String.format("https://www.jetphotos.com/showphotos.php?aircraft=all&airline=all&country-location=all&photographer-group=all&category=all&keywords-type=reg&keywords-contain=0&keywords=%s&photo-year=all&genre=all&search-type=Advanced&sort-order=0", registration);
-
-        WebClient webClient = new WebClient();
-        HtmlPage page = webClient.getPage(url);
-        DomNodeList<DomElement> images = page.getElementsByTagName("img");
-
-        List<String> photos = new ArrayList<>();
-
-        for (DomElement image : images) {
-            String src = image.getAttribute("src");
-            if(src.contains("//cdn.jetphotos.com/400/6/")){
-                photos.add(String.format("https:%s", src.replace("/400/", "/full/")));
-                break;
-            }
-        }
-
-        if(!photos.isEmpty()){
-            Page pagePhoto = webClient.getPage(photos.get(0));
-            return ResponseEntity.ok().build();
-        }else{
-            return ResponseEntity.notFound().build();
-        }
-
-
-        //ACESSAR:
-        //    https://www.jetphotos.com/showphotos.php?aircraft=all&airline=all&country-location=all&photographer-group=all&category=all&keywords-type=reg&keywords-contain=0&keywords=cc-bga&photo-year=all&genre=all&search-type=Advanced&sort-order=0
-
-        //BUSCAR PELAS TAGS NA RESPOSTA:
-        //    <img src="//cdn.jetphotos.com/400/6/75918_1492684790.jpg" class="result__photo" onerror="this.src='/assets/img/placeholders/medium.jpg'" alt="CC-BGA - Boeing 787-9 Dreamliner - LATAM Airlines " title="Photo of CC-BGA - Boeing 787-9 Dreamliner - LATAM Airlines ">
-
-        //DOWNLOAD DA IMAGEM EM:
-        //    https://cdn.jetphotos.com/full/6/75918_1492684790.jpg
-
-
-
-
-        //return ResponseEntity.ok(page.getWebResponse().getContentAsString());
-    }
-*/
 
     @GetMapping("/{registration}")
     //public RedirectView redirectByRegistration(@PathVariable String registration) throws IOException {
@@ -91,8 +36,6 @@ public class AircraftImagesController {
             }
         }
 
-        //return new RedirectView(photos.get(0));
-
         if(!photos.isEmpty()){
             AircraftImageDTO image = AircraftImageDTO.builder()
                     .registration(registration.toUpperCase())
@@ -103,6 +46,42 @@ public class AircraftImagesController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    /*
+    @GetMapping("/{registration}")
+    //public RedirectView redirectByRegistration(@PathVariable String registration) throws IOException {
+    public ResponseEntity<List<AircraftImageDTO>> redirectByRegistration(@PathVariable String registration) throws IOException {
+        String url = String.format("https://www.jetphotos.com/showphotos.php?aircraft=all&airline=all&country-location=all&photographer-group=all&category=all&keywords-type=reg&keywords-contain=0&keywords=%s&photo-year=all&genre=all&search-type=Advanced&sort-order=0", registration);
+
+        WebClient webClient = new WebClient();
+        HtmlPage page = webClient.getPage(url);
+        DomNodeList<DomElement> photos = page.getElementsByTagName("img");
+
+        List<AircraftImageDTO> images = new ArrayList<>();
+
+        for (DomElement photo : photos) {
+            String src = photo.getAttribute("src");
+            if(src.contains("//cdn.jetphotos.com/400/6/")){
+                String link = String.format("https:%s", src.replace("/400/", "/full/"));
+
+                AircraftImageDTO image = AircraftImageDTO.builder()
+                        .registration(registration.toUpperCase())
+                        .imageLink(link).build();
+
+                images.add(image);
+                //break;
+            }
+        }
+
+        //return new RedirectView(photos.get(0));
+
+        if(!photos.isEmpty()){
+            return ResponseEntity.ok(images);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+    */
 
 }
 
--- src/main/java/io/github/victorhugonf/aircraftimages/controller/dto/AircraftImageDTO.java
+++ src/main/java/io/github/victorhugonf/aircraftimages/controller/dto/AircraftImageDTO.java
@@ -1,5 +1,6 @@
 package io.github.victorhugonf.aircraftimages.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Builder
+@AllArgsConstructor
 public class AircraftImageDTO{
     private String registration;
     private String imageLink;
